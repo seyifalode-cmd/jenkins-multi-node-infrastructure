13f4c42bc9514973270df7f3a9b01bffe5da42f4
Joba Jassan <130488422+sirjobzy-kubeedge@users.noreply.github.com>
2024-11-28 19:21:56 -0500

    config(java): Set version to 17

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -51,7 +51,7 @@ resource "aws_instance" "jenkins-master" {
   # Execute a script on a remote resource
   provisioner "remote-exec" {
     inline = [
-      "sudo yum update -y && sudo yum install ansible -y && sudo yum install java-11-amazon-corretto -y",
+      "sudo yum update -y && sudo yum install ansible -y && sudo yum install java-17-amazon-corretto-headless -y",
       "sleep 60s",
       "ansible-playbook install_jenkins_master.yaml"
     ]
